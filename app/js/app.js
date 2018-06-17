13c0022d6e130886978ffbb6e5ed36bda7c24130
voznyuk.md <voznyuk.md@gmail.com>
2018-06-17 16:51:25 +0300

    analiz iframe

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,11 @@
-var p = new Parallax('.parallax_scene1',{
+var p0 = new Parallax('.parallax_scene0',{
+  offsetYBounds: 50,
+  intensity: 20,
+  center: 0.5,
+  safeHeight: 0.15
+}).init();
+
+var p1 = new Parallax('.parallax_scene1',{
   offsetYBounds: 50,
   intensity: 50,
   center: 0.5,
